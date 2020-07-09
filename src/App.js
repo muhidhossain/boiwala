62bb829d841d86a861dc508b894c570f57c59791
Muhid Hossain <muhidhossain7@gmail.com>
2020-07-09 23:48:37 +0600

    navBar cpmponent added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Boiwala</h1>
+      <NavBar></NavBar>
     </div>
   );
 }
